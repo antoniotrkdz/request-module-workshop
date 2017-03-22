a018a65ec7d23664b23ba8dde8378fd4d067dc0b
antoniotrkdz <antoniotrkdz@gmail.com>
2017-03-22 11:18:10 +0000

    solution by Antonio

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 "use strict";
 
 const request = require('request');
+const http = require('http');
 
-const myRequest = () => {
+const myRequest = (url, cb) => {
   /*
   create your own request module here.
   It should take a url to make a http GET request, and a callback function with three arguments;
@@ -10,7 +11,18 @@ const myRequest = () => {
   2. response(Object; includes the response & statusCode of the request),
   3. body (String; includes the body of the request)
   */
-}
+  http.get(url, (res) => {
+  let data = '';
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+  res.on('end', () => {
+    cb(null, res, data);
+  });
+}).on('error', (err) => {
+  cb(err);
+});
+};
 
 // Helper
 const testRequest = (module) => {
